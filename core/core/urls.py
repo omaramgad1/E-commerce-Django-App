7a9665fa72c80e04e57bd0ddded8b245e7dedcef
omaramgad1 <omaramgad89@gmail.com>
2023-05-03 06:49:18 +0300

    init django project

--- core/core/urls.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('watch/', include('watchlist_app.api.urls')),
-    path('account/', include('user_app.api.urls')),
-]
