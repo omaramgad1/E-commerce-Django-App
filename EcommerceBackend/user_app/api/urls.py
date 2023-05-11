fc7a9474ac49ac6aabc813bdd3989acbaa7e4aa3
omaramgad1 <omaramgad89@gmail.com>
2023-05-11 19:35:22 +0300

    finish custom user model , serialzers , views and routes

---
+++ EcommerceBackend/user_app/api/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+from .views import *
+urlpatterns = [
+    path('register/', register, name='register'),
+    path('login/', login, name='login'),
+    path('profile/', profile_get, name='profile'),
+    path('profile_update/', profile_update, name='profile_update'),
+    path('change_password/', change_password, name='change_password'),
+    path('list-all/', get_all_users, name='get_all_users'),
+    path('list-active/', get_active_users, name='get_all_users'),
+
+    path('<int:user_id>/change_active_status/',
+         change_user_active_status, name='change_user_active_status'),
+    path('<int:user_id>/change_to_admin/', change_user_superuser_status,
+         name='change_user_superuser_status'),
+    # path('forgot_password/', forgot_password, name='forgot_password'),
+    # path('reset_password/', reset_password, name='reset_password'),
+
+]
