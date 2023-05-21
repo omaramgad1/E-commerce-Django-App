8e066af98a5ff188d93db5bfcaabcc9a56d6e51a
omaramgad1 <omaramgad89@gmail.com>
2023-05-21 04:31:17 +0300

    add description to category model

--- EcommerceBackend/category/models.py
+++ EcommerceBackend/category/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True, null=True)
 
     class Meta:
         verbose_name_plural = 'Categories'
